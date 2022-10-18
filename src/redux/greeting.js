6731b238f6c69a8e420275335d9259126e238760
Elson Otake <elsonotake@yahoo.com.br>
2022-10-18 17:21:27 -0300

    Add axios access control

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 
 const FETCH = 'hello-react-front-end/FETCH';
 
+const config = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
 const greetingReducer = (state = [], action = {}) => {
   switch (action.type) {
     case FETCH:
@@ -15,7 +21,7 @@ const greetingReducer = (state = [], action = {}) => {
 export const loadData = createAsyncThunk(
   FETCH,
   async (_, { dispatch }) => {
-    const res = await axios.get('/api/v1/greetings')
+    const res = await axios.get('http://localhost:3000/api/v1/greetings', config)
       .then(
         (data) => dispatch({
           type: FETCH,
